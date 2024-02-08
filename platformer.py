b3f2d338eb5beff166cf1e7640d5605a2229d53b
Igor <nikitinigorra@gmail.com>
2024-02-08 21:08:33 +0200

    added world class

--- platformer.py
+++ platformer.py
@@ -10,14 +10,60 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Platformer")
 
 # load images
+sun_img = pygame.image.load("D:/Programmin in VS/GitHub/SomeStuff/pygame/platformer/images/sun.png")
+bg_img = pygame.image.load("D:/Programmin in VS/GitHub/SomeStuff/pygame/platformer/images/sky.png")
+
+class World():
+    def __init__(self, data):
+        self.tile_list = []
+       
+        #load data
+        dirt_img = pygame.image.load("D:/Programmin in VS/GitHub/SomeStuff/pygame/platformer/images/dirt.png")
+        grass_img = pygame.image.load("D:/Programmin in VS/GitHub/SomeStuff/pygame/platformer/images/grass.png")
+
+        row_count = 0
+        for row in data:
+            col_count = 0
+            for tile in row:
+                if tile == 1:
+                    img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
+                    img_rect = img.get_rect()
+                    img_rect.x = col_count * tile_size
+                    img_rect.y = row_count * tile_size
+                    tile = (img, img_rect)
+                    self.tile_list.append(tile)
+                col_count += 1
+            row_count += 1
+
+    def draw(self):
+        for tile in self.tile_list:
+            screen.blit(tile[0], tile[1])
+            
+
+world_data = [
+[1, 1, 1, 1, 1],
+[1, 0, 0, 0, 1],
+[1, 0, 0, 0, 1],
+[1, 0, 0, 0, 1],
+[1, 1, 1, 1, 1],
+]
 
 
+world = World(world_data)
 
 run = True
 while run:
     
+    screen.blit(bg_img, (0, 0))
+    screen.blit(sun_img, (100, 100))
+
+    world.draw()
+    print(world.tile_list)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
+    pygame.display.update()
+    
 pygame.quit()
